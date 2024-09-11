bd9bcef59f4bd6f9e7b350f52433de45f1437d02
coderquokka <ahnwia@gmail.com>
2024-09-11 21:27:45 +0200

    240911-2

---
+++ operation/op2_rotate.c
@@ -0,0 +1,38 @@
+#include "../includes/push_swap.h"
+#include "stack_op.h"
+
+void	rotate(t_node **stack)
+{
+	t_node	*second_last;
+	t_node	*last;
+
+	if (!stack || !*stack || !(*stack)->right)
+		return;
+	last = *stack;
+	//*stack is updated
+	*stack = (*stack)->right;
+	(*stack)->left = NULL;
+	second_last = ft_last_node(*stack);
+	second_last->right = last;
+	last->left = second_last;
+	last->right = NULL;
+}
+
+void	rotate_a(t_stack_var *var)
+{
+	rotate(&var->stack_a);
+	write(1, "ra\n", 3);
+}
+
+void	rotate_b(t_stack_var *var)
+{
+	rotate(&var->stack_b);
+	write(1, "rb\n", 3);
+}
+
+void	rotate_ab(t_stack_var *var)
+{
+	rotate_a(var);
+	rotate_b(var);
+	//write(1, "rr\n", 3);
+}
---
+++ operation/op3_rev_rotate.c
@@ -0,0 +1,36 @@
+#include "../includes/push_swap.h"
+#include "stack_op.h"
+
+//maybe wrong, check
+void	rev_rotate(t_node **stack)
+{
+	t_node	*prev_tail;
+
+	if (!stack || !*stack || !(*stack)->right)
+		return ;
+	prev_tail = ft_last_node(*stack);
+	prev_tail->right = *stack;
+	(*stack)->left = prev_tail;
+	*stack = ft_second_last_node(*stack, prev_tail);
+	(*stack)->right = NULL;
+	prev_tail->left = NULL;
+}
+
+void	rev_rotate_a(t_stack_var *var)
+{
+	rev_rotate(&var->stack_a);
+	write(1, "rra\n", 4);
+}
+
+void	rev_rotate_b(t_stack_var *var)
+{
+	rev_rotate(&var->stack_b);
+	write(1, "rrb\n", 4);
+}
+
+void	rev_rotate_ab(t_stack_var *var)
+{
+	rev_rotate_a(var);
+	rev_rotate_b(var);
+	write(1, "rrr\n", 4);
+}
